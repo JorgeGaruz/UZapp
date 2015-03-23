cf1ead125d57b3a941895458195fdd1a49c3eeb8
JorgeGaruz <sdfgenki@hotmail.com>
2015-03-23 13:15:15 +0100

    Añadido opciones de menu

--- web/www/js/app.js
+++ web/www/js/app.js
@@ -4,7 +4,7 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers'])
+angular.module('starter', ['ionic', 'starter.controllers', 'angularSlideables'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -29,43 +29,52 @@ angular.module('starter', ['ionic', 'starter.controllers'])
     templateUrl: "templates/menu.html",
     controller: 'AppCtrl'
   })
-
-  .state('app.search', {
-    url: "/search",
+  .state('splash', {
+    url: "/app/splash",
+    templateUrl: "templates/splash.html",
+    controller: 'AppCtrl'
+  })
+  
+  .state('app.home', {
+    url: "/home",
     views: {
-      'menuContent': {
-        templateUrl: "templates/search.html"
+      'menuContent':{
+        templateUrl: "templates/home.html",
+        controller: 'AppCtrl'
       }
     }
   })
-
-  .state('app.browse', {
-    url: "/browse",
-    views: {
-      'menuContent': {
-        templateUrl: "templates/browse.html"
-      }
-    }
-  })
-    .state('app.playlists', {
-      url: "/playlists",
-      views: {
-        'menuContent': {
-          templateUrl: "templates/playlists.html",
-          controller: 'PlaylistsCtrl'
-        }
-      }
-    })
-
-  .state('app.single', {
-    url: "/playlists/:playlistId",
-    views: {
-      'menuContent': {
-        templateUrl: "templates/playlist.html",
-        controller: 'PlaylistCtrl'
-      }
-    }
-  });
+  
+  .state('app.busqueda', {
+	url: "/busqueda",
+	views: {
+	  'menuContent':{
+	    templateUrl: "templates/busqueda.html",
+	    controller: 'AppCtrl'
+	      }
+	    }
+	  })
+	  
+	  .state('app.favoritos', {
+		url: "/favoritos",
+		views: {
+		  'menuContent':{
+		    templateUrl: "templates/favoritos.html",
+		    controller: 'AppCtrl'
+		      }
+		    }
+		  })
+		  
+	.state('app.ajustes', {
+		url: "/ajustes",
+		views: {
+		  'menuContent':{
+		    templateUrl: "templates/ajustes.html",
+		    controller: 'AppCtrl'
+		      }
+		    }
+		  })
+;
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/playlists');
+  $urlRouterProvider.otherwise('/app/home');
 });
--- web/www/js/controllers.js
+++ web/www/js/controllers.js
@@ -1,48 +1,23 @@
-angular.module('starter.controllers', [])
+(function(){
+  var app = angular.module('starter.controllers',[]);
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
-  // Form data for the login modal
-  $scope.loginData = {};
+  /**********************************************************************
+   * AppCtrl: Controlador principal de la aplicación.
+   ***********************************************************************/
+  app.controller('AppCtrl', function($scope, $timeout, $state) {
 
-  // Create the login modal that we will use later
-  $ionicModal.fromTemplateUrl('templates/login.html', {
-    scope: $scope
-  }).then(function(modal) {
-    $scope.modal = modal;
+    // Timeout para ir a home
+    $timeout(function() {
+      $state.go('app.home');
+    }, 2000);
   });
 
-  // Triggered in the login modal to close it
-  $scope.closeLogin = function() {
-    $scope.modal.hide();
-  };
+  /**************************************************************************
+   * TopCtrl: Controlador encargado de redirigir la aplicación a la pantalla
+   *          de splash en caso de refresco de página
+   ***********************************************************************/
+  app.controller('TopCtrl', function($location){
+    $location.path("/");
+  });
 
-  // Open the login modal
-  $scope.login = function() {
-    $scope.modal.show();
-  };
-
-  // Perform the login action when the user submits the login form
-  $scope.doLogin = function() {
-    console.log('Doing login', $scope.loginData);
-
-    // Simulate a login delay. Remove this and replace with your login
-    // code if using a login system
-    $timeout(function() {
-      $scope.closeLogin();
-    }, 1000);
-  };
-})
-
-.controller('PlaylistsCtrl', function($scope) {
-  $scope.playlists = [
-    { title: 'Reggae', id: 1 },
-    { title: 'Chill', id: 2 },
-    { title: 'Dubstep', id: 3 },
-    { title: 'Indie', id: 4 },
-    { title: 'Rap', id: 5 },
-    { title: 'Cowbell', id: 6 }
-  ];
-})
-
-.controller('PlaylistCtrl', function($scope, $stateParams) {
-});
+})();
